9bde81e8ede2209cda33a3888217742b451fd126
deno750 <deronjic.denis@gmail.com>
2020-05-19 11:21:37 +0200

    First commit

---
+++ src/lab6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
