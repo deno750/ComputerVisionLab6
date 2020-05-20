d7aaee731d6f2e3359fbcc27910682564b850443
deno750 <deronjic.denis@gmail.com>
2020-05-20 12:02:40 +0200

    test commit

--- src/lab6.cpp
+++ src/lab6.cpp
@@ -5,9 +5,20 @@
 #include <opencv2/core/utils/filesystem.hpp>
 #include <opencv2/xfeatures2d/nonfree.hpp>
 
+#include <unistd.h>
+
 using namespace std;
 
 int main() {
-    
+    cv::VideoCapture videoCapture("data/video.mov");
+    if (videoCapture.isOpened()) {
+        for (;;) {
+            cv::Mat frame;
+            videoCapture >> frame;
+            cv::imshow("VIDEO", frame);
+            break;
+        }
+    }
+    cv::waitKey(0);
     return 0;
 }
