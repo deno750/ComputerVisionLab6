59f5213d433bd73436e5e13af732396f576b6396
Francesco Marchetti <francescomarchetti92@gmail.com>
2020-05-20 12:30:55 +0200

    storia infinita

--- src/lab6.cpp
+++ src/lab6.cpp
@@ -6,8 +6,33 @@
 #include <opencv2/xfeatures2d/nonfree.hpp>
 
 using namespace std;
+using namespace cv::xfeatures2d;
+
 
 int main() {
     
+    cv::VideoCapture cap("../../src/data/video.mov");
+    if(cap.isOpened()) //check if we success
+    {
+        for(;;)
+        {
+            cv::Mat frame;
+            cap >> frame;
+            cap.read(frame);
+            // check if we succeeded
+            if (frame.empty()) {
+                cerr << "ERROR! blank frame grabbed\n";
+                break;
+            }
+            // show live and wait for a key with timeout long enough to show images
+            imshow("Live", frame);
+            if (cv::waitKey(5) >= 0)
+                break;
+        }
+    }
+    
+    Ptr<SIFT> detector = SIFT::create();
+    std::vector<KeyPoint> keypoints;
+    detector->detect( , keypoints );
     return 0;
 }
